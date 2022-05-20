fd2d955a858d894e3b12e883b93979ce2a5fdf10
ryanrliao <ryanrliao@tencent.com>
2022-05-20 14:07:07 +0800

    --other=hive-exec适配guava 27.0-jre、jackson-databind 2.11.1

--- common/src/java/org/apache/hadoop/hive/common/JvmMetricsInfo.java
+++ common/src/java/org/apache/hadoop/hive/common/JvmMetricsInfo.java
@@ -18,48 +18,53 @@
 
 package org.apache.hadoop.hive.common;
 
-import com.google.common.base.Objects;
-
+import com.google.common.base.MoreObjects;
 import org.apache.hadoop.metrics2.MetricsInfo;
 
 /**
  * JVM and logging related metrics info instances. Ported from Hadoop JvmMetricsInfo.
  */
 public enum JvmMetricsInfo implements MetricsInfo {
-  JvmMetrics("JVM related metrics etc."), // record info
-  // metrics
-  MemNonHeapUsedM("Non-heap memory used in MB"),
-  MemNonHeapCommittedM("Non-heap memory committed in MB"),
-  MemNonHeapMaxM("Non-heap memory max in MB"),
-  MemHeapUsedM("Heap memory used in MB"),
-  MemHeapCommittedM("Heap memory committed in MB"),
-  MemHeapMaxM("Heap memory max in MB"),
-  MemMaxM("Max memory size in MB"),
-  GcCount("Total GC count"),
-  GcTimeMillis("Total GC time in milliseconds"),
-  ThreadsNew("Number of new threads"),
-  ThreadsRunnable("Number of runnable threads"),
-  ThreadsBlocked("Number of blocked threads"),
-  ThreadsWaiting("Number of waiting threads"),
-  ThreadsTimedWaiting("Number of timed waiting threads"),
-  ThreadsTerminated("Number of terminated threads"),
-  LogFatal("Total number of fatal log events"),
-  LogError("Total number of error log events"),
-  LogWarn("Total number of warning log events"),
-  LogInfo("Total number of info log events"),
-  GcNumWarnThresholdExceeded("Number of times that the GC warn threshold is exceeded"),
-  GcNumInfoThresholdExceeded("Number of times that the GC info threshold is exceeded"),
-  GcTotalExtraSleepTime("Total GC extra sleep time in milliseconds");
+    JvmMetrics("JVM related metrics etc."), // record info
+    // metrics
+    MemNonHeapUsedM("Non-heap memory used in MB"),
+    MemNonHeapCommittedM("Non-heap memory committed in MB"),
+    MemNonHeapMaxM("Non-heap memory max in MB"),
+    MemHeapUsedM("Heap memory used in MB"),
+    MemHeapCommittedM("Heap memory committed in MB"),
+    MemHeapMaxM("Heap memory max in MB"),
+    MemMaxM("Max memory size in MB"),
+    GcCount("Total GC count"),
+    GcTimeMillis("Total GC time in milliseconds"),
+    ThreadsNew("Number of new threads"),
+    ThreadsRunnable("Number of runnable threads"),
+    ThreadsBlocked("Number of blocked threads"),
+    ThreadsWaiting("Number of waiting threads"),
+    ThreadsTimedWaiting("Number of timed waiting threads"),
+    ThreadsTerminated("Number of terminated threads"),
+    LogFatal("Total number of fatal log events"),
+    LogError("Total number of error log events"),
+    LogWarn("Total number of warning log events"),
+    LogInfo("Total number of info log events"),
+    GcNumWarnThresholdExceeded("Number of times that the GC warn threshold is exceeded"),
+    GcNumInfoThresholdExceeded("Number of times that the GC info threshold is exceeded"),
+    GcTotalExtraSleepTime("Total GC extra sleep time in milliseconds");
 
-  private final String desc;
+    private final String desc;
 
-  JvmMetricsInfo(String desc) { this.desc = desc; }
+    JvmMetricsInfo(String desc) {
+        this.desc = desc;
+    }
 
-  @Override public String description() { return desc; }
+    @Override
+    public String description() {
+        return desc;
+    }
 
-  @Override public String toString() {
-    return Objects.toStringHelper(this)
-        .add("name", name()).add("description", desc)
-        .toString();
-  }
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this)
+                .add("name", name()).add("description", desc)
+                .toString();
+    }
 }
--- llap-tez/src/java/org/apache/hadoop/hive/llap/tezplugins/metrics/LlapTaskSchedulerInfo.java
+++ llap-tez/src/java/org/apache/hadoop/hive/llap/tezplugins/metrics/LlapTaskSchedulerInfo.java
@@ -7,7 +7,7 @@
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,45 +15,45 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.apache.hadoop.hive.llap.tezplugins.metrics;
 
+import com.google.common.base.MoreObjects;
 import org.apache.hadoop.metrics2.MetricsInfo;
-
-import com.google.common.base.Objects;
 
 /**
  * Metrics information for llap task scheduler.
  */
 public enum LlapTaskSchedulerInfo implements MetricsInfo {
-  SchedulerMetrics("Llap task scheduler related metrics"),
-  SchedulerClusterNodeCount("Number of nodes in the cluster"),
-  SchedulerExecutorsPerInstance("Total number of executor threads per node"),
-  SchedulerMemoryPerInstance("Total memory for executors per node in bytes"),
-  SchedulerCpuCoresPerInstance("Total CPU vCores per node"),
-  SchedulerDisabledNodeCount("Number of nodes disabled temporarily"),
-  SchedulerPendingTaskCount("Number of pending tasks"),
-  SchedulerSchedulableTaskCount("Current slots available for scheduling tasks"),
-  SchedulerSuccessfulTaskCount("Total number of successful tasks"),
-  SchedulerRunningTaskCount("Total number of running tasks"),
-  SchedulerPendingPreemptionTaskCount("Total number of tasks pending for pre-emption"),
-  SchedulerPreemptedTaskCount("Total number of tasks pre-empted"),
-  SchedulerCompletedDagCount("Number of DAGs completed");
+    SchedulerMetrics("Llap task scheduler related metrics"),
+    SchedulerClusterNodeCount("Number of nodes in the cluster"),
+    SchedulerExecutorsPerInstance("Total number of executor threads per node"),
+    SchedulerMemoryPerInstance("Total memory for executors per node in bytes"),
+    SchedulerCpuCoresPerInstance("Total CPU vCores per node"),
+    SchedulerDisabledNodeCount("Number of nodes disabled temporarily"),
+    SchedulerPendingTaskCount("Number of pending tasks"),
+    SchedulerSchedulableTaskCount("Current slots available for scheduling tasks"),
+    SchedulerSuccessfulTaskCount("Total number of successful tasks"),
+    SchedulerRunningTaskCount("Total number of running tasks"),
+    SchedulerPendingPreemptionTaskCount("Total number of tasks pending for pre-emption"),
+    SchedulerPreemptedTaskCount("Total number of tasks pre-empted"),
+    SchedulerCompletedDagCount("Number of DAGs completed");
 
-  private final String desc;
+    private final String desc;
 
-  LlapTaskSchedulerInfo(String desc) {
-    this.desc = desc;
-  }
+    LlapTaskSchedulerInfo(String desc) {
+        this.desc = desc;
+    }
 
-  @Override
-  public String description() {
-    return desc;
-  }
+    @Override
+    public String description() {
+        return desc;
+    }
 
-  @Override
-  public String toString() {
-    return Objects.toStringHelper(this)
-        .add("name", name()).add("description", desc)
-        .toString();
-  }
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this)
+                .add("name", name()).add("description", desc)
+                .toString();
+    }
 }
